6c691ea8db0b540d53c72e4c594763d37a718c35
Lenawesome <nawilldominate@gmail.com>
2021-05-18 19:18:40 +0700

    Update lession 7 Lab

--- Lesson7_Lab/script.js
+++ Lesson7_Lab/script.js
@@ -1,28 +1,25 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     const COLOR_LIMIT_VALUE = 255;
     initialize();
-    
 
-
-
-    function initialize(){
+    function initialize() {
         setRandomColorList();
         bindResetEvent();
     }
 
-
-    function bindResetEvent(){
-        $('#btn-reset').on('click', function(){
+    function bindResetEvent() {
+        $('#btn-reset').on('click', function() {
+            $(".success-check, .fail-x").hide();
             setRandomColorList();
         });
     }
 
     function setRandomColorList() {
-    
+
         let circleList = $('.circle');
         let correctAnswer = randomValue(circleList.length - 1);
-    
+
         for (let i = 0; i < circleList.length; i++) {
             $(circleList[i]).off("click");
             let red = randomValue(COLOR_LIMIT_VALUE);
@@ -37,28 +34,30 @@ $(document).ready(function () {
             bindFailedEvent(circleList[i]);
         }
     }
-    
 
-    function bindSuccessEvent(circle){
-        $(circle).bind('click', function(){
-            alert("You choose wisely!!!");
+    function bindSuccessEvent(circle) {
+        $(circle).on('click', function() {
+            $(".fail-x, .success-check").hide();
+            setTimeout(function() {
+                $(".success-check").show();
+            }, 100);
         });
     }
 
-    function bindFailedEvent(circle){
-        $(circle).bind('click', function(){
-            alert("You choose poorly!!!");
+    function bindFailedEvent(circle) {
+        $(circle).on('click', function() {
+            $(".success-check, .fail-x").hide();
+            setTimeout(function() {
+                $(".fail-x").show();
+            }, 100);
         });
     }
 
     function randomValue(lastNumber) {
         return Math.floor(Math.random() * lastNumber);
     };
-    
+
     function setCircleColor(circle, red, green, blue) {
         $(circle).css("background-color", `rgb(${red}, ${green}, ${blue})`);
     }
-
-    
 });
-
