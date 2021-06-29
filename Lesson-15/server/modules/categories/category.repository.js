96f5626a2ca8e63d2e967adb79227762c21b0f72
anhlne <nawilldominate@gmail.com>
2021-06-29 19:30:10 +0700

    Update server

---
+++ Lesson-15/server/modules/categories/category.repository.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const CategorySchema = mongoose.Schema({
+    name: String,
+    description: String,
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }]
+});
+
+const Category = mongoose.model('Category', CategorySchema);
+
+const find = function() {
+    return Category.find({}).exec();
+}
+
+const findById = function(id) {
+    return Category.findById(id).exec();
+}
+
+const create = function(inputs, cb) {
+    const newCategory = new Category(inputs);
+
+    return newCategory.save();
+}
+
+const update = function(id, newObject) {
+    return Category.updateOne({ _id: id }, { $set: newObject });
+}
+
+const remove = function(id) {
+    return Category.deleteOne({ _id: id })
+}
+
+module.exports = {
+    find: find,
+    findById: findById,
+    create: create,
+    update: update,
+    remove: remove
+};
---
+++ Lesson-15/server/modules/categories/category.router.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const service = require('./category.service');
+
+router.get('/', async function(req, res) {
+    try {
+        const data = await service.find();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.post('/', async function(req, res) {
+    try {
+        const result = await service.create(req.body);
+        res.json(result);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.put('/:id', async function(req, res) {
+    try {
+        await service.update(req.params.id, req.body);
+        res.send("OK");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.delete('/:id', async function(req, res) {
+    try {
+        await service.remove(req.params.id);
+        res.send('OK');
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+module.exports = {
+    router: router
+};
